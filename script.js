226f59e85b5693b6c272f9293151b56fa81c5cfd
Tejpal singh Solanki <99065007+teju4tech@users.noreply.github.com>
2025-08-03 03:54:15 +0530

    Yes

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Ek Sikshak ','Jimmedar Naagrik'],
+    strings:['Ek Web developer ','Jimmedar Naagrik'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
